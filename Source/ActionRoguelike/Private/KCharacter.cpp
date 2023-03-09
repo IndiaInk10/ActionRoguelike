8dd1b246f8d39ea6bc112c959f5852010d4eaea2
IndiaInk10 <indiaink10@gmail.com>
2023-03-09 15:16:42 +0900

    Improved KCharacter Input
    - Improved interaction between Input and Camera
    - Improved MoveForward/MoveRight Input

--- Source/ActionRoguelike/Private/KCharacter.cpp
+++ Source/ActionRoguelike/Private/KCharacter.cpp
@@ -4,6 +4,8 @@
 #include "KCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
 
 // Sets default values
 AKCharacter::AKCharacter()
@@ -12,10 +14,15 @@ AKCharacter::AKCharacter()
 	PrimaryActorTick.bCanEverTick = true;
 
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp"); // Pointer에 변수 할당
+	SpringArmComp->bUsePawnControlRotation = true;
 	SpringArmComp->SetupAttachment(RootComponent); // Character의 루트 component에 부착하기
 	
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
 	CameraComp->SetupAttachment(SpringArmComp);
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	
+	bUseControllerRotationYaw = false;
 }
 
 // Called when the game starts or when spawned
@@ -25,17 +32,31 @@ void AKCharacter::BeginPlay()
 	
 }
 
-void AKCharacter::MoveForward(float value)
-{
-	AddMovementInput(GetActorForwardVector(), value);
-}
 
-// Called every frame
+// This include is added at the top of your KCharacter.cpp file
+#include "DrawDebugHelpers.h"
+// This is entirely optional, it draws two arrows to visualize rotations of the player
 void AKCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// -- Rotation Visualization -- //
+	const float DrawScale = 100.0f;
+	const float Thickness = 5.0f;
+
+	FVector LineStart = GetActorLocation();
+	// Offset to the right of pawn
+	LineStart += GetActorRightVector() * 100.0f;
+	// Set line end in direction of the actor's forward
+	FVector ActorDirection_LineEnd = LineStart + (GetActorForwardVector() * 100.0f);
+	// Draw Actor's Direction
+	DrawDebugDirectionalArrow(GetWorld(), LineStart, ActorDirection_LineEnd, DrawScale, FColor::Yellow, false, 0.0f, 0, Thickness);
+
+	FVector ControllerDirection_LineEnd = LineStart + (GetControlRotation().Vector() * 100.0f);
+	// Draw 'Controller' Rotation ('PlayerController' that 'possessed' this character)
+	DrawDebugDirectionalArrow(GetWorld(), LineStart, ControllerDirection_LineEnd, DrawScale, FColor::Green, false, 0.0f, 0, Thickness);
 }
+
 
 // Called to bind functionality to input
 void AKCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -43,6 +64,33 @@ void AKCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &AKCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AKCharacter::MoveRight);
+	
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+}
+
+void AKCharacter::MoveForward(float value)
+{
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Pitch = 0.0f;
+	ControlRot.Roll = 0.0f;
+	
+	AddMovementInput(ControlRot.Vector(), value);
+}
+void AKCharacter::MoveRight(float value)
+{
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Pitch = 0.0f;
+	ControlRot.Roll = 0.0f;
+
+	// Left Handed
+	// X = Forward(Red)
+	// Y = Right(Green)
+	// Z = Up(Blue)
+	
+	FVector RightVector = FRotationMatrix(ControlRot).GetScaledAxis(EAxis::Y);
+	
+	AddMovementInput(RightVector, value);
 }
 
